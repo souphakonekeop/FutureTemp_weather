262b69d2c0ade1c68ef6d1f16a04a4fd1d849ba6
99292677 <115576381+99292677@users.noreply.github.com>
2024-12-04 04:17:35 +0700

    Update FutureTemp_weather.py

--- FutureTemp_weather.py
+++ FutureTemp_weather.py
@@ -1,5 +1,3 @@
-import subprocess
-import sys
 import os
 import pandas as pd
 import numpy as np
@@ -8,27 +6,6 @@ from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM, Dropout
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import streamlit as st
-
-# Automatically install missing libraries
-required_libraries = [
-    "kaggle",
-    "streamlit",
-    "plotly",
-    "pandas",
-    "numpy",
-    "keras",
-    "tensorflow",
-    "scikit-learn",
-]
-try:
-    for library in required_libraries:
-        try:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", library])
-        except subprocess.CalledProcessError:
-            print(f"Retrying installation for {library} with --user flag...")
-            subprocess.check_call([sys.executable, "-m", "pip", "install", library, "--user"])
-except Exception as e:
-    print(f"Failed to install packages: {e}")
 
 # Kaggle API setup
 os.environ["KAGGLE_CONFIG_DIR"] = os.getcwd()  # Ensure kaggle.json is in the current directory
